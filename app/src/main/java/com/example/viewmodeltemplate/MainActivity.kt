5d373cc3180c486b971fc62448d448d39391d22c
HusainLokhandwala14 <75034342+HusainLokhandwala14@users.noreply.github.com>
2022-09-19 11:09:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viewmodeltemplate/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.viewmodeltemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodeltemplate.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var viewModel : MainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+val txt : TextView = findViewById(R.id.text)
+        txt.text = viewModel.number.toString()
+
+
+
+
+ val btn : Button = findViewById(R.id.button)
+
+        btn.setOnClickListener{
+            viewModel.addone()
+            txt.text = viewModel.number.toString()
+        }
+
+    }
+
+
+}
